d515e41a196bdfdfeaab46c821a2c2da2b07eeb0
Romain Lanz <romain.lanz@slynova.ch>
2017-11-08 16:04:03 +0100

    test(request): simple test around Request generation

--- test/Request.spec.js
+++ test/Request.spec.js
@@ -5,6 +5,20 @@
  */
 
 const test = require('japa')
+const { Request } = require('../dist/sdk.cjs')
 
 test.group('Request', () => {
+
+  test('should use the given type', (assert) => {
+    const request = new Request({ type: 'TestingType' })
+
+    assert.equal(request.toString(), '<Request Type="TestingType"></Request>')
+  })
+
+  test('should wrap when asked', (assert) => {
+    const request = new Request({ type: 'TestingType', wrapped: true })
+
+    assert.equal(request.toString(), '<Requests><Request Type="TestingType"></Request></Requests>')
+  })
+
 })
